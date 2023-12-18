b454474e495bb1b95362d815c6654ac276780ed3
SarahGuarneri <sarahguarneri97@icloud.com>
2023-12-18 17:02:41 +0100

    User class

--- BW-Team1/src/main/java/entities/User.java
+++ BW-Team1/src/main/java/entities/User.java
@@ -1,4 +1,72 @@
 package entities;
 
+@Entity
 public class User {
+    @Id
+    @GeneretedValue
+    private UUID card;
+    private String name;
+    private String surname;
+    private int age;
+    private Pass pass;
+    private List <Ticket>
+
+    public User() {}
+    public User(String name, String surname, int age, Pass pass) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.pass = pass;
+    }
+
+    public UUID getCard() {
+        return card;
+    }
+
+    public void setCard(UUID card) {
+        this.card = card;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Pass getPass() {
+        return pass;
+    }
+
+    public void setPass(Pass pass) {
+        this.pass = pass;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "card=" + card +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", pass=" + pass +
+                '}';
+    }
 }
