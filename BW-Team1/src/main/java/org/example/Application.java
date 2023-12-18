12f9e9688ca1244dbc87c8c1b0917b2c3164e096
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-12-18 14:26:47 +0100

    First commit

---
+++ BW-Team1/src/main/java/org/example/Application.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-Team1");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        EntityManager em = emf.createEntityManager();
+    }
+}
