fbcf3450d030383afd006d663f0e040d16d79ac9
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-12-19 10:26:18 +0100

    Create Tram.java

---
+++ BW-Team1/src/main/java/org/example/entities/Tram.java
@@ -0,0 +1,33 @@
+package org.example.entities;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "trams")
+@DiscriminatorValue("Tram")
+public class Tram extends Vehicle{
+    @Column(name = "numero_vagoni")
+    private int numeroVagoni;
+
+    public Tram(){
+
+    }
+
+    public int getNumeroVagoni() {
+        return numeroVagoni;
+    }
+
+    public void setNumeroVagoni(int numeroVagoni) {
+        this.numeroVagoni = numeroVagoni;
+    }
+
+    @Override
+    public String toString() {
+        return "Tram{" +
+                "numeroVagoni=" + numeroVagoni +
+                '}';
+    }
+}
