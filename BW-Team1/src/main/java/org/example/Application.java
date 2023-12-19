3a413841ff2e2322914c047cd0413c785662e6f0
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-12-19 10:26:29 +0100

    Update Application.java

--- BW-Team1/src/main/java/org/example/Application.java
+++ BW-Team1/src/main/java/org/example/Application.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.DAO.VehicleDAO;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -11,5 +13,14 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         EntityManager em = emf.createEntityManager();
+        VehicleDAO vd = new VehicleDAO(em);
+
+        //vd.save();
+
+
+      /* DELETE
+        vd.findVehicleByUUID();
+          System.out.println("Vehicle deleted");
+       */
     }
 }
